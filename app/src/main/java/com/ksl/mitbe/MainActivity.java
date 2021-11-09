47a1099d85fb673633b6c527d01485c6105312b6
andimohsoreang <andimohamaddd@gmail.com>
2021-11-10 01:46:42 +0800

    Login Screen -1234

--- app/src/main/java/com/ksl/mitbe/MainActivity.java
+++ app/src/main/java/com/ksl/mitbe/MainActivity.java
@@ -3,14 +3,26 @@ package com.ksl.mitbe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String tes;
-    String andi;
+    TextView tv;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) tv.findViewById(R.id.textView);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hallo", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

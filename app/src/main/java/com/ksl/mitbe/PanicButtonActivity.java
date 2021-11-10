e427900b8bfda91ca51cf5a6d373be3ae5a942a4
andimohsoreang <andimohamaddd@gmail.com>
2021-11-10 20:17:53 +0800

    Signin,signUp, Pb Activity and mail

---
+++ app/src/main/java/com/ksl/mitbe/PanicButtonActivity.java
@@ -0,0 +1,14 @@
+package com.ksl.mitbe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PanicButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panic_button);
+    }
+}

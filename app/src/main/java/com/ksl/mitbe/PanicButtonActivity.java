a50802a0af43301fcab69efff533f7069f08781e
andimohsoreang <andimohamaddd@gmail.com>
2021-11-11 23:54:49 +0800

    No Login

--- app/src/main/java/com/ksl/mitbe/PanicButtonActivity.java
+++ app/src/main/java/com/ksl/mitbe/PanicButtonActivity.java
@@ -10,5 +10,6 @@ public class PanicButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panic_button);
+
     }
 }
